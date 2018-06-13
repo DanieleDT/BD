82731ad63bbd67608fc90b578f6c99fbad82739d
danieledt <danieledt@Xubuntu-16.04>
2018-06-13 20:40:58 +0200

    init

--- src/csv/InitDatabase.java
+++ src/csv/InitDatabase.java
@@ -11,23 +11,30 @@ public class InitDatabase {
 		//caricamento filamenti
 		csvManager = new CsvManager(path + "/filamenti_Spitzer.csv");
 		csvManager.uploadFile(1);
+		System.out.println("filamenti spitzer caricati");
 		csvManager = new CsvManager(path + "/filamenti_Herschel.csv");
 		csvManager.uploadFile(1);
+		System.out.println("filamenti herschel caricati");
 		
 		//caricamento contorni dei filamenti
 		csvManager = new CsvManager(path + "/contorni_filamenti_Herschel.csv");
 		csvManager.uploadFile(0);
+		System.out.println("contorni filamenti herschel caricati");
 		csvManager = new CsvManager(path + "/contorni_filamenti_Spitzer.csv");
 		csvManager.uploadFile(0);
+		System.out.println("contorni filamenti spitzer caricati");
 		
 		//caricamento scheletro dei filamenti
 		csvManager = new CsvManager(path + "/scheletro_filamenti_Herschel.csv");
 		csvManager.uploadFile(2);
+		System.out.println("scheletro filamenti herschel caricati");
 		csvManager = new CsvManager(path + "/scheletro_filamenti_Spitzer.csv");
 		csvManager.uploadFile(2);
+		System.out.println("scheletro filamenti spitzer caricati");
 		
 		//caricamento stelle
 		csvManager = new CsvManager(path + "/stelle_Herschel.csv");
 		csvManager.uploadFile(3);
+		System.out.println("stelle caricate");
 	}
 }
